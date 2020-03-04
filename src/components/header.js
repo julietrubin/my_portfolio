c32511d60ecbbafc7bc85e4b446986a7727c8c68
Juliet <rubin.juliet@gmail.com>
2020-03-04 12:22:19 -0700

    Split up site into projects

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
+
 import mystyles from './header.module.scss'
 
 
 const Header = () => {
     return (
         <header className={mystyles.header}>
+            <div className={mystyles.name}>Juliet Rubin</div>
+            <Link className={mystyles.link} to="/">home</Link>
+            <Link className={mystyles.link} to="/contact">contact</Link>
+            <Link className={mystyles.link} to="/projects">projects</Link>
             <a className={mystyles.link} href="https://gitconnected.com/julietrubin/resume" target="_blank">resume</a>
         </header>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import Repository from "../components/repository"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Contact from "../components/contact"
 import AboutMe from "../components/aboutme"
 
 
 const IndexPage = ({ data }) => {
-  const {
-    name,
-    avatarUrl,
-    repositories,
-  } = data.githubData.data.viewer
+  const {avatarUrl, name} = data.githubData.data.viewer;
 
   return (
     <Layout>
       <SEO title={`${name} repos`} />
       <AboutMe avatarUrl={avatarUrl} />
-
-      <div style={{ maxWidth: `960px`, marginBottom: `1.45rem` }}>
-        <Contact />
-        {repositories.nodes
-          .map(repo => <Repository key={repo.name} repo={repo} />)
-          .reverse()}
-      </div>
     </Layout>
   )
 }
@@ -39,28 +26,6 @@ export const gitHubQuery = graphql`
         viewer {
           name
           avatarUrl
-          repositories {
-            nodes {
-              name
-              description
-              homepageUrl
-              resourcePath
-              forkCount
-              createdAt
-              updatedAt
-              languages {
-                edges {
-                  node {
-                    name
-                    color
-                  }
-                }
-              }
-              stargazers {
-                totalCount
-              }
-            }
-          }
         }
       }
     }
