8f35ab0bad4f433e88d63fb70f776cc132e35587
Juliet <rubin.juliet@gmail.com>
2020-03-04 17:10:32 -0700

    contact form is still broken

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,100 +12,95 @@ const encode = (data) => {
       .join('&')
   } 
 
-const ContactForm = () => {
+const Contact = () => {
     return (
         <div>
-        <h1>contact me</h1>
+            <h1>contact me</h1>
 
-        <Formik
-            initialValues={{ name: '', email: '', message: '' }}
-            validationSchema={validationSchema}
-            onSubmit={(values, { setSubmitting, setFieldValue, resetForm }) => {
-                fetch("/?no-cache=1", {   
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                    body: encode({
-                        'form-name': 'contact',
-                        ...values,
-                    }),
-                })
-                .then(() => {
-                    resetForm();
-                    setSubmitting(false)
-                    setFieldValue('success', true)
-                })
-                .catch(error => {
-                    alert('Error: Please Try Again!')                            
-                    setSubmitting(false)
-                })
-            }}
-            render={({
-                touched,
-                errors,
-                isSubmitting,
-                handleSubmit,
-                values,
-                setFieldValue,
-            }) => (
-                <form className='form'
-                    name='contact'
-                    data-netlify-recaptcha="true"
-                    onSubmit={handleSubmit}
-                    data-netlify='true'
-                    data-netlify-honeypot='bot-field'
-                >
-                    <div className='field'>
-                        <Field
-                            className={mystyles.input}
-                            type='text'
-                            name='name'
-                            placeholder='Name'
-                        />
-                        {touched.name && errors.name && <p className={mystyles.error}>{errors.name}</p>}
-                    </div>
-                    <div className='field'>
-                        <Field
-                            className={mystyles.input}
-                            type='text'
-                            name='email'
-                            placeholder='Email'
-                        />
-                        {touched.email && errors.email && <p className={mystyles.error}>{errors.email}</p>}
-                    </div>
-                    <div className='field'>
-                        <Field
-                            className={mystyles.input}
-                            name='message'
-                            component='textarea'
-                            rows='6'
-                        />
-                        {touched.message && errors.message && <p className={mystyles.error}>{errors.message}</p>}
-                    </div>
-                    {values.name && values.email && values.message && (
-                        <div>
-                            <Field
-                                component={Recaptcha}
-                                sitekey={RECAPTCHA_KEY}
-                                name="recaptcha"
-                                onChange={value => setFieldValue('recaptcha', value)}
-                            />
-                            {errors.recaptcha && isSubmitting && <p className={mystyles.error}>{errors.recaptcha}</p>}
-                        </div>
-                    )}
-                    <div className='buttons'>
-                        <input name='submit' type='submit' disabled={isSubmitting} value='Send'
-                        className={mystyles.button} />
-                    </div>
-                    {values.success && (
-                        <p className={mystyles.success}>
-                            Your message has been sent.
-                        </p>
-                    )}
-                </form>
-            )}
-        />
+            <Formik
+                initialValues={{ name: '', email: '', message: '' }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { setSubmitting, setFieldValue, resetForm }) => {
+                    fetch("/?no-cache=1", {   
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                        body: encode({
+                            'form-name': 'contact',
+                            ...values,
+                        }),
+                    })
+                    .then(() => {
+                        resetForm();
+                        setSubmitting(false)
+                        setFieldValue('success', true)
+                    })
+                    .catch(error => {
+                        alert('Error: Please Try Again!')                            
+                        setSubmitting(false)
+                    })
+                }}
+                render={(data) => ( <ContactForm {...data} /> )}
+            />
         </div>
   )
 }
-
-export default ContactForm
+const ContactForm = ({touched, errors, isSubmitting, handleSubmit, values, setFieldValue}) => {
+    return (
+        <form className='form'
+            name='contact'
+            data-netlify-recaptcha="true"
+            onSubmit={handleSubmit}
+            data-netlify='true'
+            data-netlify-honeypot='bot-field'
+        >
+            <div className='field'>
+                <Field
+                    className={mystyles.input}
+                    type='text'
+                    name='name'
+                    placeholder='Name'
+                />
+                {touched.name && errors.name && <p className={mystyles.error}>{errors.name}</p>}
+            </div>
+            <div className='field'>
+                <Field
+                    className={mystyles.input}
+                    type='text'
+                    name='email'
+                    placeholder='Email'
+                />
+                {touched.email && errors.email && <p className={mystyles.error}>{errors.email}</p>}
+            </div>
+            <div className='field'>
+                <Field
+                    className={mystyles.input}
+                    name='message'
+                    component='textarea'
+                    rows='6'
+                />
+                {touched.message && errors.message && <p className={mystyles.error}>{errors.message}</p>}
+            </div>
+            {values.name && values.email && values.message && (
+                <div>
+                    <Field
+                        component={Recaptcha}
+                        sitekey={RECAPTCHA_KEY}
+                        name="recaptcha"
+                        onChange={value => setFieldValue('recaptcha', value)}
+                    />
+                    {errors.recaptcha && isSubmitting && <p className={mystyles.error}>{errors.recaptcha}</p>}
+                </div>
+            )}
+            <div className='buttons'>
+                <input name='submit' type='submit' disabled={isSubmitting} value='Send'
+                className={mystyles.button} />
+            </div>
+            {values.success && (
+                <p className={mystyles.success}>
+                    Your message has been sent.
+                </p>
+            )}
+        </form>
+    )
+}
+export default Contact
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,6 @@ import React from "react"
 import Layout from "../components/layout"
 import Contact from "../components/contact"
 
-
 const ContactPage = () => {
   return (
     <Layout>
