e434d67e546c8a401530c323f4e67b58b9f8812a
Juliet <rubin.juliet@gmail.com>
2020-03-07 18:46:27 -0700

    test deploy

--- src/components/landing/aboutme/index.js
+++ src/components/landing/aboutme/index.js
@@ -11,14 +11,14 @@ const AboutMe = ({avatarUrl}) => {
     return (
         <DeviceLayout styles={styles}>
             <h1 className={styles.header}>about me</h1>
-            <div className={styles.content}>
-                <div className={styles.text}>I'm a Software Engineer.... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</div>
-                <img
-                    src={avatarUrl}
-                    alt="avatar"
-                    className={styles.avatar}
-                />
-                            </div>
+                <div className={styles.content}>
+                    <div className={styles.text}>I'm a Software Engineer.... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</div>
+                    <img
+                        src={avatarUrl}
+                        alt="avatar"
+                        className={styles.avatar}
+                    />
+                </div>
 
                 <FollowMe />
                 {isMobile && (<Link to="/contact" className={styles.link}>contact me</Link>)}
