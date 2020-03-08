1562ca2cf069c628b89abbb8655ce3bb771f4187
Juliet <rubin.juliet@gmail.com>
2020-03-07 18:37:04 -0700

    mobile and desktop look good

--- src/components/landing/aboutme/index.js
+++ src/components/landing/aboutme/index.js
@@ -11,16 +11,19 @@ const AboutMe = ({avatarUrl}) => {
     return (
         <DeviceLayout styles={styles}>
             <h1 className={styles.header}>about me</h1>
-            <div className={styles.text}>I'm a Software Engineer.... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</div>
-            <img
-                src={avatarUrl}
-                alt="avatar"
-                className={styles.avatar}
-            />
-            <FollowMe />
-            {isMobile && (<Link to="/contact">contact me</Link>)}
-            {isMobile && (<Link to="/projects">view projects</Link>)}
-            {isMobile && (<a className={styles.link} href="https://gitconnected.com/julietrubin/resume" target="_blank">view resume</a>)}
+            <div className={styles.content}>
+                <div className={styles.text}>I'm a Software Engineer.... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</div>
+                <img
+                    src={avatarUrl}
+                    alt="avatar"
+                    className={styles.avatar}
+                />
+                            </div>
+
+                <FollowMe />
+                {isMobile && (<Link to="/contact" className={styles.link}>contact me</Link>)}
+                {isMobile && (<Link to="/projects" className={styles.link}>view projects</Link>)}
+                {isMobile && (<a className={styles.link} href="https://gitconnected.com/julietrubin/resume" target="_blank" className={styles.link}>view resume</a>)}
         </DeviceLayout>
     );
 }
