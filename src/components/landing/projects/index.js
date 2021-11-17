e8b0f52797c0a2daa657809bc0c9cffe2dea863d
Juliet Rubin <julietrubin@me.com>
2021-11-16 17:16:58 -0800

    add new project

--- src/components/landing/projects/index.js
+++ src/components/landing/projects/index.js
@@ -10,6 +10,31 @@ const Projects = ({repositories}) => {
         <DeviceLayout styles={styles}>
             <h1>projects</h1>
             <div className={styles.grid}>
+                <div className={styles.repository}>
+                <div >
+                    <h3>
+                        <a
+                        href={`https://insidestudio54.com`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        Inside Studio 54
+                        </a>
+                    </h3>
+                    <div className={styles.description}>
+                        <p>
+                        Built with Webflow 
+
+                        {/* {repo.homepageUrl && (
+                            <>
+                            {" -"} <a href={repo.homepageUrl}>{repo.homepageUrl}</a>
+                            </>
+                        )} */}
+                        </p>
+                    </div>
+                    
+                    </div>
+                </div>
                 {repositories.nodes
                 .map(repo => <Repository key={repo.name} repo={repo} />)
                 .reverse()}
